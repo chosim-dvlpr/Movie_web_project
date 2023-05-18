d5d876d9c3ed4da57f6e54b582366dd688966a9e
이동현 <ldh940825@naver.com>
2023-05-18 13:47:32 +0900

    CHORE : Add API KEY & bring data.

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,34 +13,55 @@
         <router-link :to="{ name: 'Login' }">Login</router-link> 
       </span>
     </div>
+    <p v-for="movie in movieList" :key="movie.id">{{ movie.title }}</p>
     <router-view @login="isLogin=true"/> <!--라우터뷰는 클릭시 주어진 URL과 일치하는 컴포넌트를 렌더링, 장고의 block tag 와 비슷하다. -->
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import api_key from '../data'
+
 export default {
   name: 'App',
   data: function () {
     return {
       isLogin: false,
-      // isLogin 데이터 속성 정의, 초기값을 false로 설정. 
-      // 이 데이터 속성은 로그인 상태를 나타내는 변수로 사용.
+      api_key: api_key.API_KEY,
+      movieList: [],
     }
   },
   methods: {
+    showMovies: function() {
+      axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko&page=1&sort_by=popularity.desc&api_key=${this.api_key}`
+      })
+      .then(res => {
+        this.movieList = res.data.results
+        // console.log(this.movieList)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
     logout: function() {                  // logout 메서드 정의하는데, 로그아웃을 처리하기 위해 사용.
       this.isLogin = false                // isLogin 을 false 로 설정.
       localStorage.removeItem('jwt')      // 로컬스토리지에서 jwt 제거
       this.$router.push({name: 'login'})  // 로그인 페이지로 이동
     }
   },
-// 추가
-  created: function () {                        // 앱이 생성될떄 호출되는 함수 정의(라이프사이클훅)
+
+  created () {                        // 앱이 생성될떄 호출되는 함수 정의(라이프사이클훅)
     const token = localStorage.getItem('jwt')   // 로컬스토리지에서 jwt(JSON Web Token을 나타내며, 로그인한 사용자를 인증하는 데 사용되는 토큰) 값을 가져와서 token이라는 변수에 할당.
     if (token) {                                // 토큰에 값이 있다면
       this.isLogin = true                       // isLogin 을 false 에서 true 로 변환     
     }
-  }
+  },
+
+  mounted() {
+    this.showMovies()
+  },
 }
 </script>
 
--- client/vue.config.js
+++ client/vue.config.js
@@ -2,3 +2,20 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// module.exports = {
+//   // ...
+//   configureWebpack: {
+//     // ...
+//     plugins: [
+//       // ...
+//       new Dotenv({
+//         path: './.env.local',
+//         safe: true,
+//         systemvars: true,
+//         silent: true,
+//         defaults: false
+//       })
+//     ]
+//   }
+// };
